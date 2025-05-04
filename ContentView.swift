3e8f887c4c2678cb1a4e999487a46c83c106c91a
Muhammed <muhammedsahel12@gmail.com>
2025-05-04 21:59:41 +0400

    Push working full Xcode project

---
+++ ContentView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to دندنلي")
+            .font(.largeTitle)
+            .padding()
+    }
+}
--- DandanliApp.swift
+++ DandanliApp.swift
@@ -2,17 +2,9 @@ import SwiftUI
 
 @main
 struct DandanliApp: App {
-    @StateObject var history = MatchHistory()
-    @StateObject var uploadQueue = UploadRetryQueue()
-
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                SplashScreen()
-            }
-            .environmentObject(history)
-            .environmentObject(uploadQueue)
-            .preferredColorScheme(.light)
+            ContentView()
         }
     }
 }
